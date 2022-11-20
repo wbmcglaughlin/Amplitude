6d9e7b068b5a86651298c3b2d3b468e278be400f
wmcg0001 <wmcg0001@student.monash.edu>
2022-11-20 16:31:48 +1100

    feature(): mob repel forces

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,13 +1,15 @@
 use bevy::{
     prelude::*,
 };
+use rand::{Rng, thread_rng};
 use crate::mob::Mob;
+use crate::system_adapter::new;
 
 pub struct SimulationPlugin;
 impl Plugin for SimulationPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Wave {
-            current: 0
+            current: 4
         })
             .add_system(simulation);
     }
@@ -27,8 +29,10 @@ pub fn simulation(
     time: Res<Time>
 ) {
     let dt = time.delta_seconds();
+    let mut prng = thread_rng();
 
     if mobs.is_empty() {
+        wave.current += 1;
         println!("Spawning Wave: {}", wave.current);
         let mobs = get_wave(wave.current);
 
@@ -36,20 +40,60 @@ pub fn simulation(
             commands.spawn(PbrBundle {
                 mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
                 material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-                transform: Transform::from_xyz(0.0, 0.5,mob_index as f32 * 2.0),
+                transform: Transform::from_xyz(prng.gen::<f32>() * 20.0, 0.5,prng.gen::<f32>() * 20.0),
                 ..default()
             }).insert(Mob {
                 vel: Vec3::default(),
                 acc: Vec3::default(),
                 health: 10.0,
-                strength: 0.0
+                strength: prng.gen::<f32>()
             });
         }
     } else {
-        for (entity, mut transform, mut mob) in mobs.iter_mut() {
+        let mut forces: Vec<Vec3> = Vec::new();
+        let mut damages: Vec<f32> = Vec::new();
+        let mut positions: Vec<Vec3> = Vec::new();
+
+        for (entity1, transform1, mob1) in mobs.iter() {
+            let mut force = Vec3::default();
+            let mut damage = 0.0;
+
+            for (entity2, transform2, mob2) in mobs.iter() {
+                if entity2 != entity1 {
+                    let distance = transform2.translation - transform1.translation;
+
+                    // Attraction Force
+                    force += distance;
+
+                    // Repel Force
+                    let mut repel = 0.1 / distance;
+                    repel.y = 0.;
+
+                    force -= repel;
+                }
+            }
+
+            forces.push(force);
+            damages.push(damage);
+            positions.push(transform1.translation);
+        }
+
+        for (i, (entity, mut transform, mut mob)) in mobs.iter_mut().enumerate() {
+            mob.acc = forces[i];
+            mob.health -= damages[i] * dt;
+
             mob.update(dt);
 
-            transform.translation += mob.vel * dt;
+            let mut new_distance = mob.vel * dt;
+            let new_position = transform.translation + new_distance;
+
+            for position in &positions {
+                if new_position.distance_squared(position.clone()) < 0.5 * 0.5 {
+
+                }
+            }
+
+            transform.translation += new_distance;
 
             if mob.health <= 0.0 {
                 commands.entity(entity).despawn();
@@ -61,6 +105,6 @@ pub fn simulation(
 pub fn get_wave(
     wave: usize
 ) -> usize {
-    return 5;
+    return wave;
 }
 
